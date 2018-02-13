5fb72e15e10f5bd39d9646e101fe375837cc67bc
bobtsmsi <else.engine@gmail.com>
2018-02-13 02:15:29 +0100

    Add has_subscript_operator and a helper template
     - has_subscript_operator is made to test if a class has a subscript
     operator with a given signature.
     - the helper template split_member_func_sig help implementation of
     has_subscript_operator like templates.

--- templates.hpp
+++ templates.hpp
@@ -20,6 +20,59 @@ template <bool B, typename T = void>
 using eif = std::enable_if_t<B, T>;
 
 /**
+ * Split input signature R (C::*)(Args...) to a class C and a function signature
+ * R (Args...) with C cv-qualified same as input signature and R (Args...)
+ * unqualified and finally instantiate template template parameter from them.
+ * The idea is to help implementation of templates like has_subscript_operator
+ * by freeing it of writing specializations for unqualified / const / volatile
+ * and const-volatile signatures.
+ */
+namespace detail {
+
+template <template <typename, typename, typename> typename F, typename S>
+struct split_member_func_sig;
+
+template <template <typename, typename, typename = void> typename F, typename C, typename R, typename ... Args>
+struct split_member_func_sig<F, R (C::*)(Args...)> {
+    static constexpr bool value = F<C, R (Args...)>::value;
+};
+
+template <template <typename, typename, typename = void> typename F, typename C, typename R, typename ... Args>
+struct split_member_func_sig<F, R (C::*)(Args...) const> {
+    static constexpr bool value = F<const C, R (Args...)>::value;
+};
+
+template <template <typename, typename, typename = void> typename F, typename C, typename R, typename ... Args>
+struct split_member_func_sig<F, R (C::*)(Args...) volatile> {
+    static constexpr bool value = F<volatile C, R (Args...)>::value;
+};
+
+template <template <typename, typename, typename = void> typename F, typename C, typename R, typename ... Args>
+struct split_member_func_sig<F, R (C::*)(Args...) const volatile> {
+    static constexpr bool value = F<const volatile C, R (Args...)>::value;
+};
+
+} // namespace detail
+
+/**
+ * Test if a class has a subscript operator with a given signature.
+ */
+namespace detail {
+
+template <typename C, typename S, typename = void>
+struct has_subscript_operator_impl : std::false_type {};
+
+template <typename C, typename R, typename... Args>
+struct has_subscript_operator_impl<C, R (Args...), eif<
+std::is_same<decltype(std::declval<C>().operator[](std::declval<Args>()...)), R>::value
+>> : std::true_type {};
+
+} // namespace detail
+
+template <typename S>
+constexpr bool has_subscript_operator = detail::split_member_func_sig<detail::has_subscript_operator_impl, S>::value;
+
+/**
  * all_same
  */
 namespace detail {
